bfd4eca4e6838de49fa7c7928e7f3bd93ad17e34
Jaroslaw Marcin Iwanski <marcin@autentia.cl>
2019-06-15 23:57:42 -0400

    sample enhancement

--- examples/insert.py
+++ examples/insert.py
@@ -10,11 +10,11 @@ if __name__ == u"__main__":
     # load simple html
     frame.load_html("""<html>html</html>""")
 
-    # create div and link as child of root node (<html>)
-    div = sciter.Element.create(u"div", u"hello, world")
-
     root = frame.get_root()
-    root.insert(div, 0)
+    # create div and link as child of root node (<html>)
+    for n in xrange(0, 300):
+        div = sciter.Element.create(u"div", u"hello, world: %d" % n)
+        root.insert(div, 0)
 
     # show window and run app
     frame.run_app()
